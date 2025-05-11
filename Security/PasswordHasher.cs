f15a4d1d907a145d9f1b7bb409c97c2116278cde
ProNinjaDev <pronin-04@bk.ru>
2025-05-12 00:44:43 +0300

    Modify the User model, create a class for hashing according to PBKDF2 standard

---
+++ Security/PasswordHasher.cs
@@ -0,0 +1,37 @@
+using System.Security.Cryptography;
+
+namespace UserApi.Security {
+    public static class PasswordHasher {
+        private const int SaltSize = 16; // 128 бит
+        private const int HashSize = 32; // 256 бит
+        private const int NumIterations = 10000;
+        private const string NameAlgorithm = "PBKDF2_SHA256";
+
+        public struct HashedPasswordComponents
+        {
+            public byte[] Hash { get; init; }
+            public byte[] Salt { get; init; }
+            public int Iterations { get; init; }
+            public string Algorithm { get; init; }
+        }
+
+        public static HashedPasswordComponents GenerateHashedPassword(string password) {
+            byte[] salt = new byte[SaltSize];
+            var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(salt);
+            rng.Dispose();
+
+            var keyDeriver = new Rfc2898DeriveBytes(password, salt, NumIterations, HashAlgorithmName.SHA256);
+            byte[] hash = keyDeriver.GetBytes(HashSize);
+
+            return new HashedPasswordComponents {
+                Hash = hash,
+                Salt = salt,
+                Iterations = NumIterations,
+                Algorithm = NameAlgorithm
+            };
+        }
+
+        
+    }
+}
